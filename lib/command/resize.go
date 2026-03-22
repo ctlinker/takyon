396cc18e99f5af0d692a48a95b86783ae0d437b2
ctlinker <166874035+ctlinker@users.noreply.github.com>
2026-03-22 05:01:38 +0000

    path: revised resize command
    impl note :
    changed dx from resize [name] [size] --unit=<B|K|M|G|T|P|E>
    to resize [name] [size]<B|K|...>

--- lib/command/resize.go
+++ lib/command/resize.go
@@ -3,12 +3,13 @@ package command
 import (
 	"strconv"
 	"takyon/lib/container"
+	"takyon/lib/container/cutils"
 	"takyon/lib/ui"
 
 	"github.com/spf13/cobra"
 )
 
-var unit string
+var forceUmount bool
 
 var ResizeCMD = &cobra.Command{
 	Use:   "resize [name] [size]",
@@ -20,11 +21,31 @@ var ResizeCMD = &cobra.Command{
 		}
 
 		name := args[0]
-		size, str_err := strconv.Atoi(args[1])
+		size_str := args[1]
+
+		unit := size_str[len(size_str)-1:]
+		size, str_err := strconv.Atoi(size_str[:len(size_str)-1])
 
 		if str_err != nil {
 			ui.Warn("Invalid container size, expecting an integer")
+			ui.Error(ui.AbortErr.Error())
 			return
+		}
+
+		if cutils.IsMounted(name) {
+
+			if forceUmount {
+				err := container.UmountDiskImage(name)
+				if err != nil {
+					ui.Fatal(err.Error())
+					return
+				}
+			} else {
+				ui.Warn("The Container %s is currently mounted, umount it or use -f flag", name)
+				ui.Error(ui.AbortErr.Error())
+				return
+			}
+
 		}
 
 		err := container.ResizeDiskImage(container.ResizeDiskImageOption{
@@ -41,5 +62,5 @@ var ResizeCMD = &cobra.Command{
 }
 
 func init() {
-	ResizeCMD.Flags().StringVarP(&unit, "unit", "u", "M", "Size unit, defaut M for mega")
+	ResizeCMD.Flags().BoolVarP(&forceUmount, "forceUmount", "f", false, "Automaaticaly Umount the image")
 }
